0dcfefd3f5f6d4d7ae574a3dce18f28e819d043c
Mokshagna <mokshagnayenduri@gmail.com>
2025-09-17 10:40:04 +0530

    idk

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,7 +1,7 @@
 export const getProfileOfUser = (req, res) => {
     try{
         console.log("User profile data requested.");
-        return res.status(200).json({ message: "User profile data fetched successfully.", data: { name: "John Doe", email: "john.doe@example.com" } });
+        return res.status(200).json({ message: "User profile data fetched successfully.", data: { name: "Mokshagna Yenduri", email: "mokshagna.yenduri@example.com" } });
     } catch (error) {
         console.error("Error getting user profile data:", error.message);
         return res.status(500).json({ error: "Internal Server Error" });
